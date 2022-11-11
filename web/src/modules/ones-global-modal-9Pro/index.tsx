76c49281c860b656ac134ece4d906ca4e6e72bba
caoxing-mike <caoxing@ones.cn>
2022-11-11 16:47:01 +0800

    add: 更新销毁插件方式

--- web/src/modules/ones-global-modal-9Pro/index.tsx
+++ web/src/modules/ones-global-modal-9Pro/index.tsx
@@ -2,29 +2,39 @@ import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { ConfigProvider, Modal } from '@ones-design/core';
 import './index.css'
-import { useTaskInfo } from '@ones-op/store'
+import { useTaskInfo, destoryCurrentModule } from '@ones-op/store'
 import { OPDispatch } from  '@ones-op/event'
+import { OPFetch } from '@ones-op/fetch';
+
 function App() {
   const { uuid } = useTaskInfo()
   console.log('uuid', uuid)
   useEffect(() => {
     console.log('启用成功')
   }, [])
+  const testFetch = async () => {
+    try{
+      const res = await OPFetch('/hello')
+      console.log('res', res)
+    } catch(e) {
+      console.log('222222')
+    }
+  }
   return <>
     <Modal
       closable
       onCancel={function noRefCheck(){
-        OPDispatch('ones:event:global:manual:module:destroy')
+        destoryCurrentModule()
       }}
       onMaximizeChange={function noRefCheck(){}}
       onOk={function noRefCheck(){
-        OPDispatch('ones:event:global:manual:module:destroy')
+        destoryCurrentModule()
       }}
       title="Modal 对话框"
       visible
       width={520}
     >
-      Modal 对话框
+      <button onClick={testFetch}>3哈哈3</button>
     </Modal>
   </>
 }
